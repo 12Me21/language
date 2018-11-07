0186d914828b2bc1587abe42a0255c82d177b4ed
12Me21 <12Me21.mc@gmail.com>
2018-11-07 17:21:11 -0500

    temp

--- run.c
+++ run.c
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <setjmp.h>
 #include <string.h>
+#include "alloc_init.h"
 
 jmp_buf err_ret;
 
@@ -18,6 +19,7 @@ enum Type {
 	tNone,
 };
 
+// Multiple things may point to the same String or Table
 struct String {
 	char * pointer;
 	uint32_t length;
@@ -28,6 +30,7 @@ struct Variable;
 
 // A value or variable
 // Variables just contain the extra "constraint_expression" field
+// Multiple things should not point to the same Value or Variable. EVER.
 struct Value {
 	enum Type type;
 	union {
@@ -42,7 +45,7 @@ struct Value {
 
 struct Variable {
 	struct Value value;
-	//Address constraint_expression;
+	Address constraint_expression;
 };
 
 // every distinct operator
@@ -58,13 +61,14 @@ enum Operator {
 	
 	oAdd,
 	oSubtract,
-	oPrint1,
+	oPrint,
 	oTest,
 	oAssign,
 	oHalt,
+	oTable, //table literal.
+	//<key><value><key><value>...<oTable(# of items)>
+	oIndex,
 };
-
-#define ptr(x) (x *)
 
 //this takes up a lot of space (at least 40 bytes I think)... perhaps this should just be a Value, with a special [type]
 struct Item {
@@ -72,7 +76,13 @@ struct Item {
 	union {
 		struct Value value;
 		Address address; //For example, IF uses this to skip past the code when its condition is false.
-		struct Variable * variable;
+		//struct Variable * variable; //replace with index into the variables array if you want any hope of recursion.
+		struct {
+			unsigned int scope;
+			unsigned int index;
+		};
+		unsigned int length;
+		unsigned int locals;
 	};
 };
 //ideally:
@@ -93,8 +103,43 @@ struct Item {
 struct Value stack[STACK_SIZE];
 uint32_t stack_pointer = 0;
 
+
+//struct Variable variables[256];
 struct Item code[65536];
+Address call_stack[256];
+uint32_t call_stack_pointer = 0;
+struct Variable * scope_stack[256];
+unsigned int scope_pointer = 0;
+
 Address pos = 0;
+
+void push_scope(int locals){
+	if(scope_stack_pointer >= SCOPE_STACK_SIZE)
+		die("Scope Stack Overflow\n");
+	scope_stack[scope_stack_pointer++] = malloc(sizeof(struct Variable), locals);
+	//variables will not be initialized. you must use VAR first
+	//perhaps they should be set to some known value so error checking can happen.
+}
+
+void pop_scope(){
+	if(scope_stack_pointer <= 0)
+		die("Internal Error: Scope Stack Underflow\n");
+	free(scope_stack[--call_stack_pointer]);
+}
+
+void call(Address address){
+	if(call_stack_pointer >= CALL_STACK_SIZE)
+		die("Call Stack Overflow\n");
+	call_stack[call_stack_pointer++] = pos;
+	pos = address;
+}
+
+void ret(){
+	if(call_stack_pointer <= 0)
+		die("Internal Error: Call Stack Underflow\n");
+	void call(Address address){
+	pos = call_stack[--call_stack_pointer];
+}
 
 void push(struct Value value){
 	if(stack_pointer >= STACK_SIZE)
@@ -108,22 +153,36 @@ struct Value pop(){
 	return stack[--stack_pointer];
 }
 
-void * memdup(void * source, size_t size) {
-	void * mem = malloc(size);
-	return mem ? memcpy(mem, source, size) : NULL;
+struct Value stack_get(int depth){
+	if(stack_pointer-depth < 0)
+		die("Internal Error: Stack Underflow\n");
+	return stack[stack_pointer-depth];
 }
-#define ALLOC_INIT(type, ...) memdup((type[]){ __VA_ARGS__ }, sizeof(type))
+
+void stack_discard(int amount){
+	if(stack_pointer-amount < 0)
+		die("Internal Error: Stack Underflow\n");
+	stack_pointer -= amount;
+}
+
+#include "table.c"
 
 int main(){
 	struct Variable * x = ALLOC_INIT(struct Variable, {.value = {.type = tNumber, .number = 1}});
 	x->value.variable = x;
+	struct Variable * y = ALLOC_INIT(struct Variable, {.value = {.type = tNumber, .number = 1}});
+	y->value.variable = y;
 	
-	code[0] = (struct Item){.operator = oVariable, .variable = x};
-	code[1] = (struct Item){.operator = oConstant, .value = {.type = tNumber, .number = 4}};
-	code[2] = (struct Item){.operator = oAssign};
-	code[3] = (struct Item){.operator = oVariable, .variable = x};
-	code[4] = (struct Item){.operator = oPrint1};
-	code[5] = (struct Item){.operator = oHalt};
+	unsigned int i=0;
+	
+	code[i++] = (struct Item){.operator = oScope, .locals = 2}; //required
+	code[i++] = (struct Item){.operator = oConstant, .value = {.type = tNumber, .number = 4}};
+	code[i++] = (struct Item){.operator = oConstant, .value = {.type = tNumber, .number = 8}};
+	code[i++] = (struct Item){.operator = oTable, .length = 1};
+	code[i++] = (struct Item){.operator = oConstant, .value = {.type = tNumber, .number = 4}};
+	code[i++] = (struct Item){.operator = oIndex, .length = 1};
+	code[i++] = (struct Item){.operator = oPrint, .length = 1};
+	code[i++] = (struct Item){.operator = oHalt};
 	
 	if(setjmp(err_ret)){
 		printf("error\n");
@@ -139,7 +198,10 @@ int main(){
 				push(item.value);
 				break;
 			case oVariable:
-				push(item.variable->value);
+				if(item.scope) //local var
+					push(scope_stack[scope_stack_pointer-item.scope][item.index]);
+				else //global var
+					push(scope_stack[0][item.index]);
 				break;
 			case oAdd:;
 				struct Value a = pop();
@@ -155,25 +217,77 @@ int main(){
 				struct Value variable = pop();
 				variable.variable->value = value;
 				break;
-			case oPrint1:;
-				a = pop();
-				if(a.type == tNumber){
-					printf("%f\n",a.number);
-				}else{
-					die("Type mm\n");
+			case oPrint:;
+				for(i = item.length;i>=1;i--){
+					a = stack_get(i);
+					switch(a.type){
+					case tNumber:
+						printf("%g", a.number);
+						break;
+					case tString:
+						fwrite(a.string->pointer, sizeof(char), a.string->length, stdout);
+						break;
+					case tFunction:
+						printf("function"); //maybe store the function name somewhere...
+						break;
+					case tBoolean:
+						if(a.boolean)
+							printf("true");
+						else
+							printf("false");
+						break;
+					case tTable:
+						printf("table");
+					}
+					if(i!=1)
+						printf("\t");
 				}
-				break;
-			case oTest:
-				printf("test\n");
+				stack_discard(item.length);
+				printf("\n");
 				break;
 			case oHalt:
-				die("End of program\n");
+				goto end;
+			case oTable:;
+				//this is for table literals
+				//maybe those should just compile to a bunch of VAR statements?
+				i=item.length;
+				struct Table * table = ALLOC_INIT(struct Table, {.first=NULL, .last=NULL});
+				while(i-->0){
+					a = pop();
+					b = pop();
+					table_declare(table, b, ALLOC_INIT(struct Variable, {.value = a}));
+				}
+				push((struct Value){.type = tTable, .table = table});
+				break;
+			case oIndex:; //table, key
+				b = pop(); //key
+				a = pop(); //table
+				if(a.type!=tTable)
+					die("tried to index something that wasn't a table\n");
+				push(table_lookup(a.table, b));
+				break;
+			case oCall:
+				call(item.address);
+				break;
+			case oRet:
+				ret();
+				break;
+			case oDiscard: //used after calling functions where the return value is not used.
+				pop();
+				break;
+			case oScope:
+				push_scope(item.locals);
+				break;
+			case oReturn:
+				//garbage collect here
+				pop_scope();
 				break;
 			default:
 				die("Unsupported operator\n");
 			}
 		}
 	}
+	end:
 	printf("stopped\n");
 	return 0;
 }
@@ -183,4 +297,123 @@ int main(){
 //don't get rid of the Variable struct since it's needed for keeping track of constraints etc.
 
 //Jobs for the parser:
-// - generate a list of all properties (table.property) independantly from other words
+// - generate a list of all properties (table.property) independantly from other words\
+
+//when parsing:
+// VAR A
+// DEF X
+ // VAR B
+ // DEF Y
+  // VAR C
+  // B = 1
+ // END
+ // Y
+// END
+
+//VAR A -> add "A" to top of scope stack. push variable pointer (scope = top, index = 0)
+//DEF X -> add "X" to top of scope stack. create variable X tFunction function=address. add layer to scope stack, push function things.
+// (note: this includes the number of local variables that must be created. which will have filled in after the function has been parsed (use block stack)
+//VAR B -> add "B" to top of scope stack. push variable pointer (scope = top, index = 0)
+//DEF Y -> "
+//VAR C -> add "C". push variable (scope = top, index = 0)
+//B -> search downwards in scope stack for "B". will push (scope = top-1, index=0)
+//...
+
+//Hoisting functions
+//This is nessesary to preserve the sanity of programmers.
+//not sure the best way to handle this.
+
+//calling a function
+//DEF TEST(A,B,C):END:TEST(1,2,3) is compiled to
+//1 2 3 3(because 3 arguments were passed) TEST CALL ... @TEST PUSHSCOPE{3} VAR{A} VAR{B} VAR{C} MULTIASSIGN{3}
+//CALL pops a function from the stack and calls it.
+//PUSHSCOPE pushes a scope to the scope stack, creating the right number of local variables. in this case it's 3 (because arguments are local vars)
+//MULTIASSIGN will perform multiple assignments. first it reads the stack entry before the variable list, which is the number of arguments that were passed.
+// it then assigns values to the variables, and discards everything on the stack (up to the first value passed to the function)
+
+//pushing to the scope stack:
+// this will create lots of new variables
+// the tFunction type will have a function address
+
+//when a variable is encountered in the bytecode:
+//-- use its indexes to find the Variable in the scope stack.
+//-- make a copy of its value. set the variable pointer to point at the Variable in the scope stack. (note: it's important that RETURN and other operations do not preserve that pointer)
+//-- push that to the stack.
+
+//recursion idea:
+VAR A
+DEF X
+ VAR B
+ DEF Y
+  VAR C
+  B = 1
+ END
+ Y
+END
+
+X
+
+// X is called
+//  new scope is created
+//  B is added to that scope
+//  (VAR B) -> B in bytecode points to the 0th item at the top scope.
+//  Y is called
+//   new scope is created
+//   C is added to that scope
+//   C points to 0th item at the top scope
+//   B points to 0th item in the scope 1 from the top. (note: this is different from before, but it points to the same thing)
+
+//all variables contain 2 indexes:
+//- scope (perhaps: 0=global, 1=top, 2=top-1, etc.)
+//- index (position in that list)
+//The same variable might have different scope values depending on where it's used. (see prev. example))
+
+// big scary problem:
+
+// function whatever(x)
+ // return function()
+  // print(x)
+ // end
+// end
+
+//where the heck is X stored?
+
+//solve closures later
+
+//so basically, what should happen is...
+//each value that stores a function will keep track of:
+//1: the function index (there will be a list of functions, which keeps track of their argument lists and address)
+//2: variables
+
+// function whatever(x)
+ // function whatever2()
+  // var y=3
+  // return function(z)
+   // print(x,y,z)
+  // end
+ // end
+ // return whatever2()
+// end
+
+//ok and better way to call functions:
+//<args><number of args><CALL><assignments>
+//this way, the assignments are done within the function
+//among other things
+//don't question this, please.
+
+//anyway
+//the returned function in the middle
+//during parsing, it is added to the function list just like any other function.
+//however, because it is a returned function...
+//during runtime its local variables must be captured.
+//a list of local variables is stored after the function or something
+//and during parsing, each variable which isn't specifically defined in that function is given a new index.
+//so rather than x,y,z being in all different scopes, x and y are marked as closure variables, and will be accessed from the value list stored inside the function value.
+//so x might be (scope: -1, index: 0) etc.
+//whatever() will return a value like:
+//{
+//	.type = tFunction
+//	.function = 2
+//	.vars
+
+//check worry about this later
--- table.c
+++ table.c
@@ -1,10 +1,3 @@
-//awful dictionary library
-
-// entry:
-// 1: struct Value value
-// 2: size_t size
-// 3: char * key
-
 struct Entry {
 	struct Variable variable;
 	size_t key_size;
@@ -16,12 +9,15 @@ struct Table {
 	struct Entry * first;
 	struct Entry * last;
 	//int references;
-} table_new = {.first = NULL, .last = NULL, /*.references = 0*/};
+}; //table_new = {.first = NULL, .last = NULL, /*.references = 0*/};
+
+//awful dictionary library
 
 //just use a struct Value as the key, and extract the info from that.
 // no I mean the entire value
 // except for strings which are special...
-struct Entry * table_get(struct Table * table, struct Value key){
+// ... need to fix this so it takes the type into account...
+struct Entry * table_get(struct Table * table, struct Value key, bool add){
 	char * key_data;
 	size_t key_size;
 	switch(key.type){
@@ -52,51 +48,34 @@ struct Entry * table_get(struct Table * table, struct Value key){
 			return current;
 		current=current->next;
 	}
+	if(add){
+		current = ALLOC_INIT(struct Entry, {.key_size = key_size, .key = memdup(key_data, key_size), .next = NULL});
+		if(table->last){
+			table->last->next = current;
+			table->last = current;
+		}else{ //first item added to the table
+			table->first = table->last = current;
+		}
+		return current;
+	}
 	return NULL;
 }
 
-// TODO: support a way of setting the constraint expression
-void table_set(struct Table * table, struct Value key, struct Value value){
-	char * key_data;
-	size_t key_size;
-	switch(key.type){
-	case tNumber:
-		key_data = (char *)&(key.number);
-		key_size = sizeof(double);
-		break;
-	case tString:
-		key_data = (char *)key.string->pointer;
-		key_size = key.string->length * sizeof(char);
-		break;
-	case tTable:
-		key_data = (char *)&(key.table);
-		key_size = sizeof(struct Table *);
-		break;
-	case tBoolean:
-		key_data = (char *)&(key.boolean);
-		key_size = sizeof(bool);
-		break;
-	case tFunction:
-		key_data = (char *)&(key.function);
-		key_size = sizeof(Address);
-	}
-		
-	if(table->last){
-		struct Entry * current = table_get(table, key);
-		//key exists in table already
-		if(current)
-			//fix this to not overwrite the constraint!!!
-			current->variable.value = value;
-		//new key
-		else{
-			// create new entry
-			current = ALLOC_INIT(struct Entry, {.variable = {.value = value}, .key_size = key_size, .key = memdup(key_data, key_size), .next = NULL});
-			// change old last entry to point to new entry
-			table->last->next = current;
-			// update table's last entry pointer
-			table->last = current;
-		}
-	}else{
-		table->first = table->last = ALLOC_INIT(struct Entry, {.variable = {.value = value}, .key_size = key_size, .key = memdup(key_data, key_size), .next = NULL});
-	}
+//returns a reference to the Variable stored at an index in a table.
+//(Its value can be read/written)
+//Used in normal situations.
+//Remember that the value contains a pointer back to the original variable :)
+struct Value table_lookup(struct Table * table, struct Value key){
+	struct Entry * entry = table_get(table, key, false);
+	return entry ? entry->variable.value : (struct Value){.type = tNone}; //maybe should be error
+}
+
+//creates a new table slot (or overwrites an existing one) with a variable
+//(This is the only way to modify the constraint expression)
+//Meant to be used by VAR.
+//variable must be allocated on the heap (!)
+void table_declare(struct Table * table, struct Value key, struct Variable * variable){
+	struct Entry * entry = table_get(table, key, true);
+	entry->variable = *variable;
+	entry->variable.value.variable = variable;
 }
