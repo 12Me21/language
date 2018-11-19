d5013c59ec7b56c04b52bf437c7f4d5c90a8f550
12Me21 <12Me21.mc@gmail.com>
2018-11-19 01:27:37 -0500

    test

--- alloc_init.h
+++
@@ -1,5 +0,0 @@
-void * memdup(void * source, size_t size) {
-	void * mem = malloc(size);
-	return mem ? memcpy(mem, source, size) : NULL;
-}
-#define ALLOC_INIT(type, ...) memdup((type[]){ __VA_ARGS__ }, sizeof(type))
